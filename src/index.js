d3618ae6f28817fbbe3a7d8d458c5a9cd79cf992
Enikő Veszelka <encsooo@gmail.com>
2021-03-22 16:25:09 +0100

    save before pull

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import {  UserContextProvider } from './context/myContext';
+import { UserContextProvider } from './context/myContext';
 
 ReactDOM.render(
   <UserContextProvider>
